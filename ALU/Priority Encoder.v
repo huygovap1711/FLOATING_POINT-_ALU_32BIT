5c1158f8b13537496596731c2a7e8d1200d2ccfb
huy van <bahuy1711@gmail.com>
2021-07-21 15:09:12 +0700

    first commit

---
+++ ALU/Priority Encoder.v
@@ -0,0 +1,154 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//File Name: Priority Encoder.v
+//Created By: Sheetal Swaroop Burada
+//Date: 30-04-2019
+//Project Name: Design of 32 Bit Floating Point ALU Based on Standard IEEE-754 in Verilog and its implementation on FPGA.
+//University: Dayalbagh Educational Institute
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+module priority_encoder(
+			input [24:0] ssignificand,
+			input [7:0] Exponent_a,
+			output reg [24:0] Significand,
+			output [7:0] Exponent_sub
+			);
+
+reg [4:0] shift;
+
+always @(ssignificand)
+begin
+	casex (ssignificand)
+		25'b1_1xxx_xxxx_xxxx_xxxx_xxxx_xxxx :	begin
+													Significand = ssignificand;
+									 				shift = 5'd0;
+								 			  	end
+		25'b1_01xx_xxxx_xxxx_xxxx_xxxx_xxxx : 	begin						
+										 			Significand = ssignificand << 1;
+									 				shift = 5'd1;
+								 			  	end
+
+		25'b1_001x_xxxx_xxxx_xxxx_xxxx_xxxx : 	begin						
+										 			Significand = ssignificand << 2;
+									 				shift = 5'd2;
+								 				end
+
+		25'b1_0001_xxxx_xxxx_xxxx_xxxx_xxxx : 	begin 							
+													Significand = ssignificand << 3;
+								 	 				shift = 5'd3;
+								 				end
+
+		25'b1_0000_1xxx_xxxx_xxxx_xxxx_xxxx : 	begin						
+									 				Significand = ssignificand << 4;
+								 	 				shift = 5'd4;
+								 				end
+
+		25'b1_0000_01xx_xxxx_xxxx_xxxx_xxxx : 	begin						
+									 				Significand = ssignificand << 5;
+								 	 				shift = 5'd5;
+								 				end
+
+		25'b1_0000_001x_xxxx_xxxx_xxxx_xxxx : 	begin						// 24'h020000
+									 				Significand = ssignificand << 6;
+								 	 				shift = 5'd6;
+								 				end
+
+		25'b1_0000_0001_xxxx_xxxx_xxxx_xxxx : 	begin						// 24'h010000
+									 				Significand = ssignificand << 7;
+								 	 				shift = 5'd7;
+								 				end
+
+		25'b1_0000_0000_1xxx_xxxx_xxxx_xxxx : 	begin						// 24'h008000
+									 				Significand = ssignificand << 8;
+								 	 				shift = 5'd8;
+								 				end
+
+		25'b1_0000_0000_01xx_xxxx_xxxx_xxxx : 	begin						// 24'h004000
+									 				Significand = ssignificand << 9;
+								 	 				shift = 5'd9;
+								 				end
+
+		25'b1_0000_0000_001x_xxxx_xxxx_xxxx : 	begin						// 24'h002000
+									 				Significand = ssignificand << 10;
+								 	 				shift = 5'd10;
+								 				end
+
+		25'b1_0000_0000_0001_xxxx_xxxx_xxxx : 	begin						// 24'h001000
+									 				Significand = ssignificand << 11;
+								 	 				shift = 5'd11;
+								 				end
+
+		25'b1_0000_0000_0000_1xxx_xxxx_xxxx : 	begin						// 24'h000800
+									 				Significand = ssignificand << 12;
+								 	 				shift = 5'd12;
+								 				end
+
+		25'b1_0000_0000_0000_01xx_xxxx_xxxx : 	begin						// 24'h000400
+									 				Significand = ssignificand << 13;
+								 	 				shift = 5'd13;
+								 				end
+
+		25'b1_0000_0000_0000_001x_xxxx_xxxx : 	begin						// 24'h000200
+									 				Significand = ssignificand << 14;
+								 	 				shift = 5'd14;
+								 				end
+
+		25'b1_0000_0000_0000_0001_xxxx_xxxx  : 	begin						// 24'h000100
+									 				Significand = ssignificand << 15;
+								 	 				shift = 5'd15;
+								 				end
+
+		25'b1_0000_0000_0000_0000_1xxx_xxxx : 	begin						// 24'h000080
+									 				Significand = ssignificand << 16;
+								 	 				shift = 5'd16;
+								 				end
+
+		25'b1_0000_0000_0000_0000_01xx_xxxx : 	begin						// 24'h000040
+											 		Significand = ssignificand << 17;
+										 	 		shift = 5'd17;
+												end
+
+		25'b1_0000_0000_0000_0000_001x_xxxx : 	begin						// 24'h000020
+									 				Significand = ssignificand << 18;
+								 	 				shift = 5'd18;
+								 				end
+
+		25'b1_0000_0000_0000_0000_0001_xxxx : 	begin						// 24'h000010
+									 				Significand = ssignificand << 19;
+								 	 				shift = 5'd19;
+												end
+
+		25'b1_0000_0000_0000_0000_0000_1xxx :	begin						// 24'h000008
+									 				Significand = ssignificand << 20;
+								 					shift = 5'd20;
+								 				end
+
+		25'b1_0000_0000_0000_0000_0000_01xx : 	begin						// 24'h000004
+									 				Significand = ssignificand << 21;
+								 	 				shift = 5'd21;
+								 				end
+
+		25'b1_0000_0000_0000_0000_0000_001x : 	begin						// 24'h000002
+									 				Significand = ssignificand << 22;
+								 	 				shift = 5'd22;
+								 				end
+
+		25'b1_0000_0000_0000_0000_0000_0001 : 	begin						// 24'h000001
+									 				Significand = ssignificand << 23;
+								 	 				shift = 5'd23;
+								 				end
+
+		25'b1_0000_0000_0000_0000_0000_0000 : 	begin						// 24'h000000
+								 					Significand = ssignificand << 24;
+							 	 					shift = 5'd24;
+								 				end
+		default : 	begin
+						Significand = (~ssignificand) + 1'b1;
+						shift = 8'd0;
+					end
+
+	endcase
+end
+assign Exponent_sub = Exponent_a - shift;
+
+endmodule
